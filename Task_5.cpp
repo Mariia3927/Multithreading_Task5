e0e2bedd109e8fe770a120a497f3ac226c78dac6
Mariia Shestakova <48029085+Mariia3927@users.noreply.github.com>
2019-11-27 10:44:39 +0200

    Add files via upload

---
+++ Task_5.cpp
@@ -0,0 +1,13 @@
+// Task_5.cpp : This file contains the 'main' function. Program execution begins and ends there.
+//
+
+#include "pch.h"
+#include <iostream>
+
+int main()
+{
+	Photos object;
+	object.ReadData();
+	object.OutputReadData();
+}
+
